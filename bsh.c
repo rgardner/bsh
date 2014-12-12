660f4542f8b701894b1b2c30a17f5d00e4654d32
Robert Gardner <rhg259@nyu.edu>
2014-12-11 23:15:59 -0500

    Add minimal forking example

--- bsh.c
+++ bsh.c
@@ -1,5 +1,7 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 void print_arguments(int, char**);
 
@@ -7,6 +9,25 @@ void print_arguments(int, char**);
 int main(int argc, char **argv) {
   printf("Welcome to Bob shell.\n");
   print_arguments(argc, argv);
+
+  pid_t pid;
+  if ((pid = fork()) == -1) {
+    // An error occurred...
+    exit(EXIT_FAILURE);
+  }
+
+  else if (pid == 0) {
+    // Child process
+    printf("Child: Play time!\n");
+  }
+
+  else {
+    // Parent process
+    printf("Parent: Go to bed!\n");
+    int status;
+    waitpid(pid, &status, 0);
+    printf("Child exited with status code: %d.\n", status);
+  }
   exit(EXIT_SUCCESS);
 }
 
