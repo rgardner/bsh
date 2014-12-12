bb541008dc9e2a44dc5c67e5cf580f39718ad3b8
Robert Gardner <rhg259@nyu.edu>
2014-12-12 00:53:31 -0500

    Add fgets support

--- bsh.c
+++ bsh.c
@@ -3,37 +3,66 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+void parse_command();
 void print_arguments(int, char**);
+void print_prompt();
 
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   printf("Welcome to Bob shell.\n");
   print_arguments(argc, argv);
 
-  pid_t pid;
-  if ((pid = fork()) == -1) {
-    // An error occurred...
-    exit(EXIT_FAILURE);
-  }
+  while (1) {
+    /*pid_t pid;*/
+    char cmdline[100];
 
-  else if (pid == 0) {
-    // Child process
-    printf("Child: Play time!\n");
-  }
+    print_prompt();
+    fgets(cmdline, sizeof(cmdline), stdin);  // read from stdin.
+    printf("cmd: %s", cmdline);
 
-  else {
-    // Parent process
-    printf("Parent: Go to bed!\n");
-    int status;
-    waitpid(pid, &status, 0);
-    printf("Child exited with status code: %d.\n", status);
+    /*cmdline = read_command_line();*/
+    /*cmd = parse_command(cmdline);*/
+
+    // record in history
+
+    // if builtin command(cmd), execute it.
+    // else: spawn subprocess to deal with it.
+/*    if ((pid = fork()) == -1) {*/
+      /*// An error occurred...*/
+      /*exit(EXIT_FAILURE);*/
+    /*}*/
+
+    /*else if (pid == 0) {*/
+      /*// Child process*/
+      /*printf("Child: Play time!\n");*/
+    /*}*/
+
+    /*else {*/
+      /*// Parent process*/
+      /*printf("Parent: Go to bed!\n");*/
+      /*int status;*/
+      /*waitpid(pid, &status, 0);*/
+      /*printf("Child exited with status code: %d.\n", status);*/
+    /*}*/
   }
   exit(EXIT_SUCCESS);
 }
 
+void parse_command()
+{
+}
+
 /* print arguments. */
-void print_arguments(int argc, char **argv) {
+void print_arguments(int argc, char **argv)
+{
   while (argc--) {
     printf("%s\n", *argv++);
   }
+}
+
+
+void print_prompt()
+{
+  printf("bsh: ");
 }
