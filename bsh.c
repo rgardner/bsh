24287033e9d14cf29ae2aac831cd34150e79e575
Robert Gardner <rhg259@nyu.edu>
2014-12-11 23:09:05 -0500

    Refactor print arguments into new function

--- bsh.c
+++ bsh.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_arguments(int, char**);
+
+
 int main(int argc, char **argv) {
   printf("Welcome to Bob shell.\n");
+  print_arguments(argc, argv);
+  exit(EXIT_SUCCESS);
+}
+
+/* print arguments. */
+void print_arguments(int argc, char **argv) {
   while (argc--) {
     printf("%s\n", *argv++);
   }
-  exit(EXIT_SUCCESS);
 }
