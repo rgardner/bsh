0ca3d4eaefc8de614028e9f850684d3d159484cb
Robert Gardner <rhg259@nyu.edu>
2014-12-24 17:59:49 -0800

    Fix gcc warnings; add cornell code

--- parse.c
+++ parse.c
@@ -1,33 +1,51 @@
+/**************************************************************************
+ *    parse.c - Parsing portion of my small shell
+ *    Syntax:   myshell command1 [< infile] [| command]* [> outfile] [&]
+ *************************************************************************/
+
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#include <parse.h>
-#include <types.h>
+#include "parse.h"
+
+#define MAXLINE 81
 
 void init_info(parseInfo *p) {
-  p = malloc(sizeof(parseInfo));
+  printf("init_info: initializing parseInfo\n");
 }
 
-void parse_command(char* command, struct commandType *comm) {
+void parse_command(char *command, commandType *comm) {
+  printf("parse_command: parsing a single command\n");
 }
 
-parseInfo* parse(char *cmd_line) {
-  assert(FALSE);
-  // for each cmd in cmdline {
-  //  if (cmd == command) {
-  //      parse_command(cmd, type)
-  //      }
-  //  }
+parseInfo* parse(char *cmdline) {
+  parseInfo *Result;
+  char command[MAXLINE];
+  int com_pos = -1;
+
+  if (cmdline[-1] == '\n' && cmdline[-1] == '\0') {
+    return NULL;
+  }
+
+  Result = malloc(sizeof(parseInfo));
+  init_info(Result);
+  com_pos = 0;
+  int i = 0;
+  while (cmdline[i] != '\n' && cmdline[i] != '\0') {
+    // find command position.
+    i++;
+  }
+
+  command[com_pos] = '\0';
+  parse_command(command, 0);
+  return Result;
 }
 
 void print_info(parseInfo *info) {
-  foreach type in parseInfo {
-    print "type_name: type"
-  }
+  printf("print_info: printing info about parseInfo struct\n");
 }
 
-void free_info(parse_info *info) {
-  foreach memory_block in parseInfo {
-    free(memory_block)
-  }
+void free_info(parseInfo *info) {
+  printf("free_info: freeing memory associated to parseInfo struct\n");
 }
--- parse.h
+++ parse.h
@@ -1,19 +1,21 @@
+#include "types.h"
+
 #define MAX_VAR_NUM 10
 #define PIPE_MAX_NUM 10
 #define FILE_MAX_SIZE 40
 
-struct commandType {
+typedef struct {
   char *command;
   char *VarList[MAX_VAR_NUM];
   int VarNum;
-};
+} commandType;
 
 /* The parsed command information. */
 typedef struct {
   BOOL hasInputRedirection;   // infile specified
   BOOL hasOutputRedirection;  // outfile specified
-  BOOL runInBackground:       // run in background
-  struct commandType CommArray[PIPE_MAX_NUM];
+  BOOL runInBackground;       // run in background
+  commandType CommArray[PIPE_MAX_NUM];
   int pipeNum;
   char inFile[FILE_MAX_SIZE];   // file to be piped from
   char outFile[FILE_MAX_SIZE];  // file to pipe into
