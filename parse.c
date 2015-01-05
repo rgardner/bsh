03bffa05a8f1907fd3ed39238fc453ae905c2e81
Robert Gardner <rhg259@nyu.edu>
2015-01-04 23:16:56 -0800

    Add basic command parsing

--- parse.c
+++ parse.c
@@ -12,7 +12,18 @@
 #include "parse.h"
 
 void init_info(parseInfo *p) {
-  *p = (parseInfo){ FALSE, FALSE, FALSE, {"", {""}, 0}, 1, "", "" };
+  *p = (parseInfo){ .hasInputRedirection = FALSE,
+                    .hasOutputRedirection = FALSE,
+                    .runInBackground = FALSE,
+                    .pipeNum = 0,
+                    .inFile = "",
+                    .outFile = "" };
+}
+
+void init_command(commandType *p) {
+  *p = (commandType){ .command = malloc(MAXLINE*sizeof(char)),
+                      .VarList = NULL,
+                      .VarNum = 0};
 }
 
 void parse_command(char *command, commandType *comm) {
@@ -21,74 +32,98 @@ void parse_command(char *command, commandType *comm) {
 
 parseInfo* parse(char *cmdline) {
   parseInfo *Result;
-  char command[MAXLINE];
   int i = 0;
   int com_pos = -1;
   int infile_pos = 0;
   int outfile_pos = 0;
 
-  if (cmdline[-1] == '\n' && cmdline[-1] == '\0') {
-    return NULL;
-  }
+  // Is cmdline empty?
+  if (cmdline[-1] == '\n' && cmdline[-1] == '\0') return NULL;
+
+  // Skip blank characters at the start of the cmdline
+  for (; isspace(cmdline[i]) && cmdline[i] != '\n' && cmdline[i] != '\0'; i++);
+  if (cmdline[i] == '\n' && cmdline[i] == '\0') return NULL;
 
   Result = malloc(sizeof(parseInfo));
   init_info(Result);
   com_pos = 0;
 
-  // skip blank characters at the start of the cmdline
-  for (; isspace(cmdline[i]) && cmdline[i] != '\n' && cmdline[i] != '\0'; i++);
-
   BOOL finished_command = FALSE;
   BOOL finished_infile = FALSE;
   BOOL finished_outfile = FALSE;
-  for (int i = 0; cmdline[i] != '\n' && cmdline[i] != '\0'; i++) {
-    // command1 < infile | command > outfile &
+
+  commandType *Command = malloc(sizeof(commandType));
+  init_command(Command);
+  for (; cmdline[i] != '\n' && cmdline[i] != '\0'; i++) {
     if (!finished_command) {
       if (isspace(cmdline[i])) {
         finished_command = TRUE;
+        Command->command[com_pos] = '\0';
+        Result->CommArray[Result->pipeNum] = *Command;
+        Result->pipeNum++;
       } else {
-        command[com_pos] = cmdline[i];
+        Command->command[com_pos] = cmdline[i];
         com_pos++;
       }
-    } else if (!finished_infile && Result->hasInputRedirection) {
-      if (isspace(cmdline[i])) {
-        if (strcmp(Result->inFile, "") == 0) continue;
-        finished_infile = TRUE;
-      } else {
-        Result->inFile[infile_pos] = cmdline[i];
-        infile_pos++;
-      }
-    } else if (!finished_outfile && Result->hasOutputRedirection) {
-      if (isspace(cmdline[i])) {
-        if (strcmp(Result->outFile, "") == 0) continue;
-        finished_outfile = TRUE;
-      } else {
-        Result->outFile[outfile_pos] = cmdline[i];
-        outfile_pos++;
-      }
-    } else {
-      if (cmdline[i] == '<') {
-        Result->hasInputRedirection = TRUE;
-      } else if (cmdline[i] == '>') {
-        Result->hasOutputRedirection = TRUE;
-      } else if (cmdline[i] == '&') {
-        Result->runInBackground = TRUE;
-      }
     }
+    /*// command1 < infile | command > outfile &*/
+    /*if (!finished_command) {*/
+      /*if (isspace(cmdline[i])) {*/
+        /*finished_command = TRUE;*/
+      /*} else {*/
+        /*command[com_pos] = cmdline[i];*/
+        /*com_pos++;*/
+      /*}*/
+    /*} else if (!finished_infile && Result->hasInputRedirection) {*/
+      /*if (isspace(cmdline[i])) {*/
+        /*if (strcmp(Result->inFile, "") == 0) continue;*/
+        /*finished_infile = TRUE;*/
+      /*} else {*/
+        /*Result->inFile[infile_pos] = cmdline[i];*/
+        /*infile_pos++;*/
+      /*}*/
+    /*} else if (!finished_outfile && Result->hasOutputRedirection) {*/
+      /*if (isspace(cmdline[i])) {*/
+        /*if (strcmp(Result->outFile, "") == 0) continue;*/
+        /*finished_outfile = TRUE;*/
+      /*} else {*/
+        /*Result->outFile[outfile_pos] = cmdline[i];*/
+        /*outfile_pos++;*/
+      /*}*/
+    /*} else {*/
+      /*if (cmdline[i] == '<') {*/
+        /*Result->hasInputRedirection = TRUE;*/
+      /*} else if (cmdline[i] == '>') {*/
+        /*Result->hasOutputRedirection = TRUE;*/
+      /*} else if (cmdline[i] == '&') {*/
+        /*Result->runInBackground = TRUE;*/
+      /*} else if (isspace(cmdline[i])) {*/
+        /*// end argument*/
+      /*} else {*/
+        /*// argument*/
+      /*}*/
+    /*}*/
   }
+  Command->command[com_pos] = '\0';
+  Result->CommArray[Result->pipeNum] = *Command;
+  Result->pipeNum++;
 
-  command[com_pos] = '\0';
   if (Result->hasInputRedirection) Result->inFile[infile_pos] = '\0';
   if (Result->hasOutputRedirection) Result->outFile[outfile_pos] = '\0';
 
-  parse_command(command, 0);
+  Result->pipeNum = 1;
+  parse_command(Result->CommArray[0].command, &Result->CommArray[0]);
   return Result;
 }
 
 void print_info(parseInfo *info) {
-  /*printf("prog: %s\n", info->);*/
-  // iterate through program arguments.
-  /*printf("arg%d: %s\n");*/
+  for (int i = 0; i < info->pipeNum; i++) {
+    printf("prog: %s\n", info->CommArray[i].command);
+    for (int j = 0; j < info->CommArray[i].VarNum; j++) {
+      printf("arg%d: %s\n", j, info->CommArray[i].VarList[j]);
+    }
+  }
+
   if (info->hasInputRedirection) {
     printf("inpipe: %s\n", info->inFile);
   } else {
