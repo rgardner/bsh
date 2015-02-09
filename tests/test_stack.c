7463fbe4ca7849491b85602578577d8b60760550
Robert Gardner <rhg259@nyu.edu>
2015-02-08 23:40:12 -0500

    Remove tests Makefile, update stack API in test_stack

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -1,10 +1,10 @@
 #include <string.h>
 
 #include "minunit.h"
-#include "../stack.h"
+#include "../src/stack.h"
 
 static char data[1024] = "element 1";
-static struct stack *s;
+static struct Stack *s;
 
 void
 test_setup()
@@ -23,29 +23,22 @@ MU_TEST(test_init_normal)
 
 MU_TEST(test_push_normal)
 {
-  char *elem;
-
-  elem = strndup(data, sizeof(char)*strlen(data));
+  char *elem = strndup(data, sizeof(char)*strlen(data));
   stack_push(s, elem);
   mu_assert(stack_peak(s) == elem, "element should be on the top of the stack");
 }
 
 MU_TEST(test_pop_normal)
 {
-  char *elem;
-  char *other;
-
-  elem = strndup(data, strlen(data));
+  char *elem = strndup(data, strlen(data));
   stack_push(s, elem);
-  other = stack_pop(s);
+  char *other = stack_pop(s);
   mu_assert(elem == other, "pop should remove an element");
 }
 
 MU_TEST(test_push_pop)
 {
-  char *elem;
-
-  elem = strndup(data, strlen(data));
+  char *elem = strndup(data, strlen(data));
   stack_push(s, elem);
   stack_pop(s);
   mu_assert(stack_peak(s) == NULL, "stack should be empty after push + pop");
