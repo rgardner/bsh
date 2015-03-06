53daf09364367ef37a900924070210956eb67ecc
Robert Gardner <rhg259@nyu.edu>
2015-03-06 10:54:37 -0500

    Initial commit of job code

---
+++ src/job.c
@@ -0,0 +1,36 @@
+#include "job.h"
+
+#include <unistd.h>
+
+/* The active jobs are linked into a list.  This is its head.   */
+job *first_job = NULL;
+
+/* Find the active job with the indicated pgid.  */
+job *
+find_job(pid_t pgid)
+{
+  for (job *j = first_job; j; j = j->next)
+    if (j->pgid == pgid)
+      return j;
+  return NULL;
+}
+
+/* Return true if all processes in the job have stopped or completed.  */
+int
+job_is_stopped(job *j)
+{
+  for (process *p = j->first_process; p; p = p->next)
+    if (!p->completed && !p->stopped)
+      return 0;
+  return 1;
+}
+
+/* Return true if all processes in the job have completed.  */
+int
+job_is_completed(job *j)
+{
+  for (process *p = j->first_process; p; p = p->next)
+    if (!p->completed)
+      return 0;
+  return 1;
+}
---
+++ src/job.h
@@ -0,0 +1,29 @@
+#ifndef JOB_H
+#define JOB_H
+
+#include <termios.h>
+#include <unistd.h>
+
+/* A process is a single process.  */
+typedef struct process
+{
+  struct process *next;       /* next process in pipeline */
+  char **argv;                /* for exec */
+  pid_t pid;                  /* process ID */
+  char completed;             /* true if process has completed */
+  char stopped;               /* true if process has stopped */
+  int status;                 /* reported status value */
+} process;
+
+/* A job is a pipeline of processes.  */
+typedef struct job
+{
+  struct job *next;           /* next active job */
+  char *command;              /* command line, used for messages */
+  process *first_process;     /* list of processes in this job */
+  pid_t pgid;                 /* process group ID */
+  char notified;              /* true if user told about stopped job */
+  struct termios tmodes;      /* saved terminal modes */
+  int stdin, stdout, stderr;  /* standard i/o channels */
+} job;
+#endif
