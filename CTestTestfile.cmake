c75a6aa89ed4aa0e67613d2b7c22adee93bb38dd
Robert Gardner <rhg259@nyu.edu>
2015-03-24 02:04:16 -0400

    Add check to project; split cmake config
    Add check to the project. This allows for comprehensive unit testing. To
    do this, the CMakeLists.txt was split into the `src` and `tests`
    directories, simplifying the original cmake config.

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bobgardner/Developer/bsh
+# Build directory: /Users/bobgardner/Developer/bsh
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check_bsh "/Users/bobgardner/Developer/bsh/bin/check_bsh")
+subdirs(src)
+subdirs(tests)
