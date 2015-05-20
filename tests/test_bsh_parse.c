cc4ca85109cc0c8e70983cea11aafad8db060c91
Robert Gardner <rhg259@nyu.edu>
2015-05-20 19:58:37 -0400

    Test parse file redirection

--- tests/test_bsh_parse.c
+++ tests/test_bsh_parse.c
@@ -19,6 +19,21 @@ START_TEST(test_parse_blanks) {
 }
 END_TEST
 
+START_TEST(test_parse_file_redirection) {
+  struct ParseInfo *p = parse("echo <infile >outfile\n");
+  ck_assert(p->hasInputRedirection == true);
+  ck_assert(p->hasOutputRedirection == true);
+  ck_assert(!strncmp(p->inFile, "infile", strlen("infile")));
+  ck_assert(!strncmp(p->outFile, "outfile", strlen("outfile")));
+}
+END_TEST
+
+START_TEST(test_parse_free_null) {
+  // This should not segfault.
+  free_info(NULL);
+}
+END_TEST
+
 START_TEST(test_parse_print_null) {
   // This should not segfault.
   print_info(NULL);
@@ -35,6 +50,8 @@ Suite *make_parse_suite(void) {
   tcase_add_checked_fixture(tc, parse_setup, parse_teardown);
   tcase_add_test(tc, test_parse_empty);
   tcase_add_test(tc, test_parse_blanks);
+  tcase_add_test(tc, test_parse_file_redirection);
+  tcase_add_test(tc, test_parse_free_null);
   tcase_add_test(tc, test_parse_print_null);
 
   return s;
