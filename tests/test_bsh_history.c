58bffa5e10f56c890602db33ca1a5551753346b7
Robert Gardner <rhg259@nyu.edu>
2015-05-25 19:17:12 -0700

    Test history expansion with new command

--- tests/test_bsh_history.c
+++ tests/test_bsh_history.c
@@ -5,14 +5,14 @@
 #include "test_bsh.h"
 #include "../src/history.h"
 
-void history_setup(void) {
-  history_init();
-}
+void history_setup(void) { history_init(); }
 
 void history_teardown(void) {}
 
-START_TEST(test_history_init) {
-
+START_TEST(test_history_exp_new_command) {
+  char *expansion;
+  const int his_res = history_exp("new_command", &expansion);
+  ck_assert(his_res == 0);
 }
 END_TEST
 
@@ -24,7 +24,7 @@ Suite *make_history_suite(void) {
 
   suite_add_tcase(s, tc);
   tcase_add_checked_fixture(tc, history_setup, history_teardown);
-  tcase_add_test(tc, test_history_init);
+  tcase_add_test(tc, test_history_exp_new_command);
 
   return s;
 }
