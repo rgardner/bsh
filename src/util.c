1237c8bf056d2b14b6d833cda5361ddd245f0000
Robert Gardner <rhg259@nyu.edu>
2016-02-20 14:19:00 -0500

    Simplify and improve testing of trim_right

--- src/util.c
+++ src/util.c
@@ -1,17 +1,23 @@
 #include "util.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char*
-trim(char* s)
+/** @brief Remove trailing whitespace
+ *
+ *  @return New length of s.
+ */
+size_t
+trim_right(char* s)
 {
-  const int i = strlen(s) - 1;
-  if ((i > 0) && (s[i] == '\n')) {
-    s[i] = '\0';
+  int i = strlen(s) - 1;
+  while ((i >= 0 && isspace(s[i]))) {
+    i--;
   }
-  return s;
+  s[i + 1] = '\0';
+  return i + 1;
 }
 
 /** @brief Print error message for failed system/library
--- src/util.h
+++ src/util.h
@@ -1,8 +1,10 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-/* Remove trailing newline */
-char* trim(char* s);
+#include <stddef.h>
+
+/* Remove trailing whitespace */
+size_t trim_right(char* s);
 
 /* Call perror(s) and exit. */
 void handle_error(const char* s);
