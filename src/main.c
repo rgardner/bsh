e457f23e4a3cb9222c5d7c0a85f444d0118c5f99
Bob Gardner <bob.hn.gardner@gmail.com>
2019-03-09 22:37:45 -0800

    Fix reallocf on Linux

--- src/main.c
+++ src/main.c
@@ -257,7 +257,8 @@ main(int argc, char** argv)
           continue;
         }
 
-        const size_t check __attribute__((unused)) = strlcpy(cmd_line, expansion, newsz) + 1;
+        const size_t check __attribute__((unused)) =
+          strlcpy(cmd_line, expansion, newsz) + 1;
         assert(check == newsz);
       }
 
